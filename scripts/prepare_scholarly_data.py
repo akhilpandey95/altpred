7c92a3014af54716b35837df79163e58f97d9f1c
Akhil Pandey <akhilpandey95@icloud.com>
2019-04-29 23:41:12 -0500

    Issue #32: Updated scholarly data prep script (#33)
    - added function that collects the pub_date first and then tries to
    eliminate all the articles that are published prior to 2014
    - post elimination data_processing() function extracts meta information
    of all the remaining articles
    
    Signed-off-by: akhilpandey95 <akhilpandey95@icloud.com>

--- scripts/prepare_scholarly_data.py
+++ scripts/prepare_scholarly_data.py
@@ -6,7 +6,8 @@
 import sys
 import pandas as pd
 from tqdm import tqdm
-from altmetric_api import add_all_info
+from datetime import datetime
+from altmetric_api import add_all_info, add_pubdate
 
 # function for preparing the 2014 altmetrics dataset
 def data_load(file_path):
@@ -29,17 +30,70 @@ def data_load(file_path):
         data = pd.read_csv(file_path, sep='\t')
 
         # remove all the unecessary columns
-        data = data.drop(columns=['DOI', 'PMID', 'ARXIV_ID', 'HANDLE', 'FACEBOOK_WALLS', 'BLOGS', 'GPLUS_ACCOUNTS', 'NEWS_OUTLETS'])
+        data = data.drop(columns=['DOI', 'SCORE', 'PMID', 'ARXIV_ID', 'HANDLE', 'FACEBOOK_WALLS', 'BLOGS', 'GPLUS_ACCOUNTS', 'NEWS_OUTLETS'])
 
         # return the dataframe
         return data
     except:
         return pd.DataFrame()
 
+# function for returning year from an epoch
+def get_year_from_epoch(epoch):
+    """
+    Obtain the year from a given epoch string
+
+    Parameters
+    ----------
+    arg1 | epoch: str
+        Epoch string
+
+    Returns
+    -------
+    Number
+        int
+
+    """
+    return datetime.datetime.fromtimestamp(epoch).year
+
+# function for eliminating unecessary rows
+def eliminate_row_by_pubdate(data_frame):
+    """
+    Process the dataset and collect all the meta infromation for the 2014 altmetrics dataset
+
+    Parameters
+    ----------
+    arg1 | data_frame: pandas.DataFrame
+        A loaded dataframe with just ID, twitter count
+
+    Returns
+    -------
+    Dataframe
+        pandas.DataFrame
+
+    """
+    try:
+        # first collect the pubdate of every article
+        data_frame = data_frame.assign(pub_date = [add_pubdate(x) for x in tqdm(data_frame['ID'])])
+
+        # drop the row whose row value is an empty string
+        data_frame = data_frame[data_frame.pub_date != '']
+
+        # remove the rows whose year of publishing is prior to 2014
+        data_frame = data_frame[date_frame['pub_date'].apply(get_year_from_epoch) == 2014]
+
+        # reset the dataframe index
+        data_frame = data_frame.reset_index(drop=True)
+        
+        # return the modified dataframe
+        return data_frame
+    except:
+        # return the original dataframe
+        return data_frame
+    
 # function for processing the dataset
 def data_processing(data_frame):
     """
-    The 
+    Process the dataset and collect all the meta infromation for the 2014 altmetrics dataset
 
     Parameters
     ----------
@@ -54,7 +108,7 @@ def data_processing(data_frame):
     """
     try:
         # add all the information from the dataframe
-        result = [add_all_info(x) for x in tqdm(data_frame['ID'])]
+        result = [ignore_and_return_result(add_all_info(x), ['pub_date']) for x in tqdm(data_frame['ID'])]
 
         # concat the dataframes
         data_frame = pd.concat([data_frame, pd.DataFrame(result)], axis=1)
@@ -68,6 +122,9 @@ def data_processing(data_frame):
 if __name__ == '__main__':
     # read the data
     data = data_load('altmetrics_j2014_full.csv')
+
+    # eliminate the articles that are published prior to 2014
+    data = eliminate_row_by_pubdate(data)
 
     # process the dataframe
     data = data_processing(data)
